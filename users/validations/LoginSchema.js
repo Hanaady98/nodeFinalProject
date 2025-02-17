dcfa662f800cea8bebedcca177d27c63b9fbd11e
Hanady <159716343+Hanaady98@users.noreply.github.com>
2025-02-17 22:35:22 +0200

    Update LoginSchema.js

--- users/validations/LoginSchema.js
+++ users/validations/LoginSchema.js
@@ -4,7 +4,7 @@ const LoginSchema = Joi.object({
     email: Joi.string().email({ tlds: { allow: false } }).required(),
     password: Joi.string().ruleset.pattern(/((?=.*\d{1})(?=.*[A-Z]{1})(?=.*[a-z]{1})(?=.*[!@#$%^&*-]{1}).{8,20})/)
         .rule({
-            message: "password must be at least nine characters long and contain an uppercase letter, a lowercase letter, a number and one of the following characters !@#$%^&*-"
+            message: "password must be at least eight characters long and contain an uppercase letter, a lowercase letter, a number and one of the following characters !@#$%^&*-"
         }).required()
 });
 
